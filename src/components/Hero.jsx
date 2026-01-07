319b8fbc15439332893182f6135f885acddbe90d
Shagufta Afrin <cloudname99@gmail.com>
2026-01-07 21:06:35 +0530

    dd

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
 
   // Mobile images (portrait – 1080x1920)
   const mobileImages = [
-    "/images/mobilebg.png",
+    "/images/13.png",
   ];
 
   const [current, setCurrent] = useState(0);
